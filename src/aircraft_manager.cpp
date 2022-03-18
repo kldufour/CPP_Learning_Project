22e6e50385016215885655cdc4d7848da8dc3f23
kdufour <kdufour@etud.u-pem.fr>
2022-03-18 19:15:51 +0100

    TASK2 : Objectif 1 -> B 1

--- src/aircraft_manager.cpp
+++ src/aircraft_manager.cpp
@@ -1,5 +1,7 @@
 #include "aircraft_manager.hpp"
 
+#include <algorithm>
+
 void AircraftManager::add(std::unique_ptr<Aircraft> aircraft)
 {
     aircrafts.emplace_back(std::move(aircraft));
@@ -7,17 +9,22 @@ void AircraftManager::add(std::unique_ptr<Aircraft> aircraft)
 
 bool AircraftManager::update()
 {
-    for (auto it = aircrafts.begin(); it != aircrafts.end();)
-    {
-        auto& aircraft = **it;
-        if (aircraft.update())
-        {
-            ++it;
-        }
-        else
-        {
-            it = aircrafts.erase(it);
-        }
-    }
+    // for (auto it = aircrafts.begin(); it != aircrafts.end();)
+    // {
+    //     auto& aircraft = **it;
+    //     if (aircraft.update())
+    //     {
+    //         ++it;
+    //     }
+    //     else
+    //     {
+    //         it = aircrafts.erase(it);
+    //     }
+    // }
+
+    aircrafts.erase(std::remove_if(aircrafts.begin(), aircrafts.end(),
+                                   [](std::unique_ptr<Aircraft>& aircraft) { return !aircraft->update(); }),
+                    aircrafts.end());
+
     return true;
 }
